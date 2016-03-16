c4ed39efd3b390a239c92948359c7010f99ccfb8
Ravishankar <ravishankargceb@gmail.com>
2016-03-16 18:44:24 +0000

    admin pages

--- common/scripts/scripts.js
+++ common/scripts/scripts.js
@@ -1,5 +1,19 @@
 $(function(){
-    $('.js-custom-cake-btn').on('click',function(){
-        
-    })
+    $('body').on('click','.dropdown-button',function(e){
+        e.preventDefault();
+        if($('#dropdown1').css('display') === 'none'){
+            $('#dropdown1').addClass('dropdown-show');
+        }else{
+             $('#dropdown1').removeClass('dropdown-show');   
+        }
+            
+    });
+
+    
+     $('body').on('click','#dropdown1 a ',function(e){
+        e.preventDefault();
+        //alert(this.text);
+        $('.dropdown-button').click();
+    });
+    
 })
--- users/member/controllers/adminDashboardController.js
+++ users/member/controllers/adminDashboardController.js
@@ -2,10 +2,58 @@ define(['app'], function (app) {
 	app.controller('adminDashboardController', ['$scope', '$http', function ($scope, $http) {
 
 		$scope.orderDetailView = false;
+		$scope.cakeDetails = {};
+		$scope.cakeOrders = {};
 		
-		$scope.toggleOrderDetails = function(){
-			$scope.orderDetailView = !$scope.orderDetailView;
+		function getObject(Arrobj,key,value){
+			for(var obj in Arrobj){
+				//for(var elm in obj){
+					if(Arrobj[obj][key] === value){
+						return Arrobj[obj];
+					}
+				//}
+			}
 		}
+		
+		$scope.toggleOrderDetails = function(temp){
+			$scope.orderDetailView = !$scope.orderDetailView;
+			if(temp !== undefined){
+				//getObject('orderId',temp);
+				$scope.selectedOrder = getObject($scope.cakeOrders,'orderId',temp);
+			}
+		}
+		
+		$scope.changeStatus = function(text){
+			//alert('etesd');
+			
+			$scope.selectedOrder.status = text;
+			switch(text){
+				case 'completed' :  completedProcess();
+					break;
+				case 'pending' : break;
+				case 'delivered' : break;
+			}
+		}
+		
+		$scope.openModal = function(e){
+			e.preventDefault();
+		}
+		
+		function completedProcess(){
+			
+		}
+		
+		$http.get('/cakeAppNew/cakeapp/mockup/orderDetails.json').success(function (response) { 
+			if(response){
+        		$scope.cakeDetails = response.details;
+			}
+        }); 
+        
+        $http.get('/cakeAppNew/cakeapp/mockup/cakeOrders.json').success(function (response) { 
+			if(response){
+        		$scope.cakeOrders = response.orders;
+			}
+        }); 
 		
 	}]);
 });
